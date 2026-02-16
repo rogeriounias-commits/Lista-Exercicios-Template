943416e5280fc4e79b7626da441b3f6ccb7a6489
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-16 20:34:47 -0300

    Exercício 4 - calculadora simples

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite o primeiro número: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+int soma = numero1 + numero2;
+int subtracao = numero1 - numero2;
+int multiplicacao = numero1 * numero2;
+int divisao = numero1 / numero2;
+
+Console.WriteLine($"Soma: {soma}");
+Console.WriteLine($"Subtração: {subtracao}");
+Console.WriteLine($"Multiplicação: {multiplicacao}");
+Console.WriteLine($"Divisão inteira: {divisao}");
+
