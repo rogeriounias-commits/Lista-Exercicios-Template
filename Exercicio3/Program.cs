8549cbe0d69cb462417681965e4c7722ac82de5c
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-16 20:24:52 -0300

    exercício 3 - soma de dois números

--- Exercicio3/Program.cs
+++ Exercicio3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite o primeiro número: ");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+int soma = numero1 + numero2;
+
+Console.WriteLine($"A soma é: {soma}");
