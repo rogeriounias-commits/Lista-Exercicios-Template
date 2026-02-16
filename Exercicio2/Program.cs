aafe88ba0b4c2c6242445043268bb0cb0ce2e2ca
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-16 17:53:31 -0300

    Exercício 2 - antecessor e sucessor

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int antecessor = numero - 1;
+int sucessor = numero + 1;
+
+Console.WriteLine($"Antecessor:{antecessor}");
+Console.WriteLine($"Sucessor:{sucessor}");
