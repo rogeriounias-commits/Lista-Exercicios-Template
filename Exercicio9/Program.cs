cf78a814b09139f5cea8d6a25b4319273dce70ea
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-17 09:10:18 -0300

    Exercício 9 - Divisão Segura try/catch

--- Exercicio9/Program.cs
+++ Exercicio9/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+try
+{
+    int n1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Digite o segundo número: ");
+    int n2 = Convert.ToInt32(Console.ReadLine());
+
+    int divisao = n1 / n2;
+
+    Console.WriteLine($"Resultado: {n1} ÷ {n2} = {divisao}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Entrada inválida. Digite apenas números.");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Erro: não é possível dividir por zero.");
+}
+
