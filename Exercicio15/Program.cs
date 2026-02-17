f1ee07fd9b86630923109f4fabc780648f48efe0
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-17 12:22:43 -0300

    Exercício 15 - simulador de compras

--- Exercicio15/Program.cs
+++ Exercicio15/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite o nome do produto: ");
+string produto = Console.ReadLine();
+
+try
+{
+    Console.Write("Digite o preço unitário: ");
+    double preco = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Digite a quantidade: ");
+    int quantidade = Convert.ToInt32(Console.ReadLine());
+
+    double total = preco * quantidade;
+
+    Console.WriteLine();
+    Console.WriteLine("=== RESUMO DA COMPRA ===");
+    Console.WriteLine($"Produto: {produto}");
+    Console.WriteLine($"Quantidade: {quantidade}");
+    Console.WriteLine($"Total: R$ {total:F2}");
+}
+catch
+{
+    Console.WriteLine("Entrada inválida. Digite apenas números para preço e quantidade.");
+}
+
