68c3dc6aec92b8b41147bc089141457afee04df4
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-17 09:31:42 -0300

    Exercício 12 - área e perímetro do retângulo

--- Exercicio12/Program.cs
+++ Exercicio12/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite a base do retângulo: ");
+double baseRet = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a altura do retângulo: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double area = baseRet * altura;
+double perimetro = 2 * (baseRet + altura);
+
+Console.WriteLine($"Área: {area}");
+Console.WriteLine($"Perímetro: {perimetro}");
