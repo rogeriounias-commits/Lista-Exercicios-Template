fe319fa3898fe6498bf92c055a9015c513940e11
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-16 23:09:53 -0300

    Exercício 5 - conversão metros para centímetros

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite o valor em metros: ");
+double metros = Convert.ToDouble(Console.ReadLine());
+
+double centimetros = metros * 100;
+
+Console.WriteLine($"Resultado:{metros} metro(s) equivalem a {centimetros} centímetros.");
