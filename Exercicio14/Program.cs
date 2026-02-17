f496fb234fce893f27e6c83692c1190c3b89a2ee
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-17 11:23:04 -0300

    Exercício 14 - minicalculadora com método

--- Exercicio14/Program.cs
+++ Exercicio14/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int Somar(int a, int b)
+{
+    return a + b;
+}
+
+Console.Write("Digite o primeiro número: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int resultado = Somar(n1, n2);
+
+Console.WriteLine($"Resultado: {resultado}");
+
