5f35c71ddc25719512f2dafe06ca9179bc5dfdaf
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-17 10:02:13 -0300

    Exercício 13 - média com F2

--- Exercicio13/Program.cs
+++ Exercicio13/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite a primeira nota: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a segunda nota: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+double media = (n1 + n2) / 2;
+
+Console.WriteLine($"Média: {media.ToString("F2")}");
+
