f891b98674aef96132a9984628ec3894cdade429
rogeriounias-commits <rogeriounias@gmail.com>
2026-02-17 09:20:33 -0300

    Exercício 11 - troco

--- Exercicio11/Program.cs
+++ Exercicio11/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite o valor do produto: ");
+double valorProduto = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o valor pago: ");
+double valorPago = Convert.ToDouble(Console.ReadLine());
+
+double troco = valorPago - valorProduto;
+
+Console.WriteLine($"Valor do troco: R$ {troco}");
